1bb4e18ff826ab34b679f85d18b13227e45421cc
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-02-21 17:24:47 +0600

    add money authenticaton system add

---
+++ script/addMoney.js
@@ -0,0 +1,37 @@
+document.getElementById("add-money-btn").addEventListener("click", function() {
+  const select = document.getElementById("select-bank")
+  const selectBank = select.value;
+  if (selectBank == "Select back") {
+    alert("Please select a bank")
+    return;
+  }
+
+  const bankAccount = document.getElementById("bank-account-number");
+  const accountNumber = bankAccount.value;
+  if (accountNumber == "01234567890") {
+  } else {
+    alert("please provide a valid account number")
+    return;
+  }
+
+  const add =document.getElementById("add-amount");
+  const addAmount = add.value;
+  if (addAmount <= 0) {
+    alert("please provide a valid amount")
+    return;
+  }
+
+  const accountPIn = document.getElementById("pin");
+  const pin = accountPIn.value;
+  if(pin == "1234") {
+
+  } else {
+    alert("please provide a valid pin")
+    return;
+  }
+
+  const elementAmount = document.getElementById("amount");
+  const currentAmount = elementAmount.innerHTML;
+  const newAmount = Number(currentAmount) + Number(addAmount);
+  elementAmount.innerText = newAmount;
+})
